07aa3a33f2170cd27fb88a7bae600bcd843309eb
Joseph Emmanuel Dayo <joseph.dayo@gmail.com>
2016-07-08 15:15:05 +0800

    Implement ruby to java calls

--- lib/brains/net.rb
+++ lib/brains/net.rb
@@ -2,9 +2,73 @@ require 'java'
 
 module Brains
   class Net
-    def initialize(input:, output:, total:)
+    def initialize(input, output, total, opts = {})
       @config = com.dayosoft.nn.NeuralNet::Config.new(input, output, total)
+      @config.bias = opts[:bias] || 1.0
+      @config.outputBias = opts[:output_bias] || 1.0
+      @config.learningRate = opts[:learning_rate] || 0.1
+      @config.neuronsPerLayer = opts[:neurons_per_layer] || 5
+      @config.momentumFactor = opts[:momentum_factor] || 0.5
+      @config.activationFunctionType = opt_to_func(opts[:activation_function] || :htan)
+      @config.outputActivationFunctionType = opt_to_func(opts[:activation_function] || :sigmoid)
+      @config.errorFormula = opt_to_error_func(opts[:activation_function] || :mean_squared)
       @nn = com.dayosoft.nn.NeuralNet.new(@config);
+    end
+
+    def randomize_weights(min = -1, max = 1)
+      @nn.randomizeWeights(min, max)
+    end
+
+    def dump
+      @nn.dumpWeights.to_a.map(&:to_a)
+    end
+
+    def optimize(test_cases, target_error = 0.01, is_batch = false)
+      inputs = []
+      outputs = []
+
+      test_cases.each do |item|
+        inputs << item[0].to_java(Java::double)
+        outputs << item[1].to_java(Java::double)
+      end
+
+      @nn.optimize(java.util.ArrayList.new(inputs), java.util.ArrayList.new(outputs), target_error, is_batch)
+    end
+
+    def feed(input)
+      @nn.feed(input.to_java(Java::double)).to_a
+    end
+
+    def to_json
+      @nn.saveStateToJson
+    end
+
+    private
+
+    def opt_to_func(func)
+      case func
+      when :htan
+        com.dayosoft.nn.Neuron::HTAN
+      when :sigmoid
+        com.dayosoft.nn.Neuron::SIGMOID
+      when :softmax
+        com.dayosoft.nn.Neuron::SOFTMAX
+      when :rectifier
+        com.dayosoft.nn.Neuron::RECTIFIER
+      else
+        raise "invalid activation function #{func}"
+      end
+    end
+
+    def opt_to_error_func(func)
+      case func
+      when :mean_squared
+        com.dayosoft.nn.NeuralNet::Config::MEAN_SQUARED
+      when :cross_entropy
+        com.dayosoft.nn.NeuralNet::Config::CROSS_ENTROPY
+      else
+        raise "Invalid Error Function #{func}"
+      end
     end
   end
 end
--- spec/brains_spec.rb
+++ spec/brains_spec.rb
@@ -5,7 +5,23 @@ describe Brains do
     expect(Brains::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it 'Train the sin function using neural networks' do
+    # 1 input neuron, 1 output neuron, 5 total neurons
+    brain = Brains::Net.new(1, 1, 20)
+    brain.randomize_weights
+    test_cases = [
+      [ [1.0] , [Math.sin(1.0)] ],
+      [ [0.9] , [Math.sin(0.9)] ],
+      [ [0.5] , [Math.sin(0.5)] ],
+      [ [0.3] , [Math.sin(0.3)] ],
+      [ [0.1] , [Math.sin(0.1)] ],
+      [ [0.2] , [Math.sin(0.2)] ],
+      [ [0.8] , [Math.sin(0.8)] ],
+      [ [0] , [Math.sin(0)] ],
+    ]
+    expect((brain.feed([0.6])[0] - Math.sin(0.6)).abs).to be > 0.1
+    brain.optimize(test_cases, 0.001, false)
+    expect((brain.feed([0.6])[0] - Math.sin(0.6)).abs).to be < 0.1
+    expect(JSON.parse(brain.to_json).select { |k,v| ['n'].include? k}).to eq ({'n' => 20})
   end
 end
