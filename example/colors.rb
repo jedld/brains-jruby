35bf40a4386bc923b7add53b27a14cf71b35a65e
Joseph Emmanuel Dayo <joseph.dayo@gmail.com>
2017-02-24 00:48:09 +0800

    Add color tests

--- example/colors.rb
+++ example/colors.rb
@@ -11,19 +11,33 @@ label_encodings = {
 }
 #0000ff
 training_data = [
+  # blue
   [[0xf0/0xff.to_f, 0xf8.to_f/0xff.to_f, 1],[0,0,1]],
   [[0x00/0xff.to_f, 0x00.to_f/0xff.to_f, 1],[0,0,1]],
-  [[0x00/0xff.to_f, 0x00.to_f/0xff.to_f, 0x8b.to_f/0xff.to_f],[0,0,1]]
+  [[0x00/0xff.to_f, 0x00.to_f/0xff.to_f, 0x8b.to_f/0xff.to_f],[0,0,1]],
+
+  #green
+  [[0x8f/0xff.to_f, 0x97.to_f/0xff.to_f, 0x79.to_f/0xff.to_f],[0,1,0]],
+  [[0x56/0xff.to_f, 0x82.to_f/0xff.to_f, 0x03.to_f/0xff.to_f],[0,1,0]],
+  [[0x1/0xff.to_f, 0x32.to_f/0xff.to_f, 0x20.to_f/0xff.to_f],[0,1,0]],
+
+  #red 	#CE2029 	#DA2C43	#DA2C43
+  [[0xce/0xff.to_f, 0x20.to_f/0xff.to_f, 0x29.to_f/0xff.to_f],[1,0,0]],
+  [[0xda/0xff.to_f, 0x20.to_f/0xff.to_f, 0x43.to_f/0xff.to_f],[1,0,0]],
+  [[0x99/0xff.to_f, 0x00.to_f/0xff.to_f, 0x00.to_f/0xff.to_f],[1,0,0]],
 ]
 
-nn = Brains::Net.create(2, 1, 9, { neurons_per_layer: 4 })
+nn = Brains::Net.create(3, 3, 2, { neurons_per_layer: 5 })
+
+# randomize weights before training
 nn.randomize_weights
 
 
 # test on untrained data
-#0000ee
+#0000ee 	#C41E3A
 test_data = [
-  [0x00/0xff.to_f, 0x00.to_f/0xff.to_f, 0xee.to_f/0xff.to_f],
+  [0x00/0xff.to_f, 0x00.to_f/0xff.to_f, 0xee.to_f/0xff.to_f], #blue
+  [0xc4/0xff.to_f, 0x1e.to_f/0xff.to_f, 0x3a.to_f/0xff.to_f], #red
 ]
 
 results = test_data.collect { |item|
@@ -32,7 +46,7 @@ results = test_data.collect { |item|
 
 p results
 
-result = nn.optimize(training_data, 0.01, 1_000 ) { |i, error|
+result = nn.optimize(training_data, 0.01, 1_000_000 ) { |i, error|
   puts "#{i} #{error}"
 }
 
